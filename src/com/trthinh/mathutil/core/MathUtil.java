ea342897015eaead4771394cf679ccdc3ece0ef2
noday-off <thinn2002@gmail.com>
2022-06-15 11:54:42 +0700

    Refactor the main code and green 4ever

--- src/com/trthinh/mathutil/core/MathUtil.java
+++ src/com/trthinh/mathutil/core/MathUtil.java
@@ -24,19 +24,44 @@ public class MathUtil {
     //20 giai thừa là 18 con số 0, vừa kịp đủ cho kiều long của java
     //21 giai thừa tràn kiều long
     //bài này quy ước tính n! trong khoảng từ 0..20
+//    public static long getFactorial(int n){
+//        if (n < 0 || n > 20){
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
+//        }
+//        if(n == 0 || n == 1){
+//            return 1; // kết thúc cuộc chơi sớm nếu nhận nhung đầu vào đặc biệt            
+//        }
+//        long product = 1; // tích nhân dồn, thuật toán heo đất, ốc bu dồn thịt
+//        
+//        for (int i = 2; i <= n; i++) {
+//            product *= i; // product = product * i;
+//        }
+//        return product;
+//    }
+    
+    //Hoc de quy trong vong 30s
+    //Hien tuong goi lai chinh minh voi 1 quy mo khac
+    //Ví dụ: con búp bê Nga, giống nhau và lồng trong nhau
+    //búp bê to, nhỏ hơn, nhỏ hơn nữa, .... đến đime63 dừng
+    
+    //tính giùm tui 6!
+    //n! = 1.2.3.4.5.6. ... .n
+    //6! = 6 x 5!
+    //5! = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1! điểm dừng
+    //1! = 1;
+    
+    //chốt hạ: n! = n * (n - 1)!
+    
     public static long getFactorial(int n){
-        if (n < 0 || n > 20){
+        if(n < 0 || n > 20){
             throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
         }
         if(n == 0 || n == 1){
             return 1; // kết thúc cuộc chơi sớm nếu nhận nhung đầu vào đặc biệt            
-        }
-        long product = 1; // tích nhân dồn, thuật toán heo đất, ốc bu dồn thịt
-        
-        for (int i = 2; i <= n; i++) {
-            product *= i; // product = product * i;
-        }
-        return product;
+        }        
+        return n * getFactorial(n-1);
     }
-    
 }
